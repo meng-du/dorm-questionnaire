d60c77a50a0392d27bbe476c19f9621b34fb0839
Meng Du <mengdu@umich.edu>
2019-09-28 20:28:08 -0700

    template

---
+++ js/login.js
@@ -0,0 +1,16 @@
+'use strict';
+
+(function () {
+    // Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyBvPWLV2yjapJKblBLcfkVbpZC3cXtM0PU",
+        authDomain: "dorm-network.firebaseapp.com",
+        databaseURL: "https://dorm-network.firebaseio.com",
+        projectId: "dorm-network",
+        storageBucket: "",
+        messagingSenderId: "804230274072",
+        appId: "1:804230274072:web:dd26c12bba4f85e64df76d"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+})();
