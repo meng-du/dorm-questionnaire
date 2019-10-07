389a6b6516d5091b217c203edd8e82870659dfe5
Meng Du <mengdu@umich.edu>
2019-10-06 23:15:11 -0700

    login page; roster creation

---
+++ js/test_roster.js
@@ -0,0 +1,7 @@
+roster = {
+    'mary-decker-a-205': null,
+    'ruby-basyouni-b-394': null,
+    'yuchen-li--2': null,  // no middle name
+    'ucla-bruin-x-200': null,
+    'oh-mygosh-o-234': null
+}
