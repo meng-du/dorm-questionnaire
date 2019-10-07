087299c5e27118330d419094c3d563c2accb8735
Meng Du <mengdu@umich.edu>
2019-10-07 00:05:10 -0700

    fixed roster creation

--- js/test_roster.js
+++ js/test_roster.js
@@ -1,7 +1,9 @@
+// key format: firstname-lastname-middleinitial(s)-dorm
+// must be all lowercase, and contain a-z and 0-9 only
 roster = {
     'mary-decker-a-205': null,
     'ruby-basyouni-b-394': null,
     'yuchen-li--2': null,  // no middle name
     'ucla-bruin-x-200': null,
-    'oh-mygosh-o-234': null
+    'oh-shoot-o-234': null
 }
