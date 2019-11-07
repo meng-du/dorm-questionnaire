11f2cc5677d611f696d2fa8b1b917cc7d189f081
Meng Du <mengdu@umich.edu>
2019-11-06 22:03:32 -0800

    extra options for roster

--- js/survey.js
+++ js/survey.js
@@ -1,11 +1,14 @@
 'use strict';
 // TODO hook window
 (function () {
-    $('.chosen-select').on('chosen:ready', function(ev, args) {
+    // set up roster based questions with Chosen
+    $('.roster-select').on('chosen:ready', function(ev, args) {
+        // always show placeholder
         var sender = args.chosen;
         sender.search_field.attr('placeholder', sender.default_text);
-      }).chosen({placeholder_text_multiple: 'Search here...'});
-    $('.chosen-select').trigger('chosen:open');
+    }).chosen({placeholder_text_multiple: 'Search here...'});
+    // always show the option list
+    $('.roster-select').trigger('chosen:open');
     $('.chosen-search-input').blur();
     window.scrollTo(0, 0);
 
@@ -23,5 +26,19 @@
     firebase.initializeApp(firebaseConfig);
     var db = firebase.firestore();
     var db_roster_name = 'test_roster';
+
+    // set up Add buttons
+    for (var i = 1; i <= 3; ++i) {
+        $('#q' + i + '-add-btn').click((e) => {
+            var id_prefix = '#' + e.target.id.substr(0, 2);
+            var new_name = $(id_prefix + '-add-input').val();
+            $(id_prefix + '-answer').append('<option selected>' + new_name + '</option>');
+            $(id_prefix + '-answer').chosen().trigger("chosen:updated");
+            $(id_prefix+ '-add-input').val('');  // clear input
+        });
+    }
+
+
+
 
 })();
