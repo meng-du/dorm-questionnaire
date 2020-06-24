f5dcb7bf6406b9573ee5c4da2f18565af7a3d52b
Meng Du <mengdu@umich.edu>
2020-06-23 17:47:34 -0700

    minor

--- js/progress.js
+++ js/progress.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function() {
     if (window.location.search.substring(1).length > 0) {
         let completed = window.location.search.substring(1).split(/[&=]/)[1];
         console.log(completed);
-        if ((Date.now() - parseInt(completed)) / 60000 > 720) {  // more than 12 hrs
+        if ((Date.now() - parseInt(completed)) / 60000 > 120) {  // more than 2 hrs
             $('#end-survey').hide();
         }
     } else {
